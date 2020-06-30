78afa4cfec59eb35b169721e1cbc9082833650e3
Marc Page <marcallenpage@gmail.com>
2020-06-30 15:03:23 -0500

    now login kind of works

--- ui.py
+++ ui.py
@@ -5,37 +5,69 @@ import argparse
 import logging
 import mako.template
 import db
-
+import os
 
 class MainHandler(tornado.web.RequestHandler):
-    def get(self):
+    def initialize(self, storage):
+        self.__storage = storage
+
+    def get(self, user_id=None):
         template = mako.template.Template(filename='ui/index.html.mako')
-        self.write(template.render(title='Welcome to Relifi Game', logged_in=False))
+        self.write(template.render(title='Welcome to Relifi Game',
+                                   logged_in=user_id))
 
 
 class LoginHandler(tornado.web.RequestHandler):
+    def initialize(self, storage):
+        self.__storage = storage
+
     def get(self):
         template = mako.template.Template(filename='ui/redirect.html.mako')
-        self.write(template.render(title='Redirect to /', redirect='/'))
+        email = self.get_body_argument('email')
+        password = self.get_body_argument('password')
+        user = self.__storage.login_user(email, password)
 
+        if not user['id']:
+            self.write(template.render(title='Redirect to /', redirect='/#bademail'))
+
+        elif not user['valid']:
+            self.write(template.render(title='Redirect to /', redirect='/#badpassword'))
+
+        else:
+            self.write(template.render(title='Redirect to /user/' + str(user['id']),
+                                       redirect='/user/' + str(user['id'])))
+
+    def post(self):
+        self.get()
 
 def parse_args():
 	parser = argparse.ArgumentParser(description='Financial Game')
 	parser.add_argument('-p', '--port', default='8000',
+	                    help='Port to listen on for http connections')
+	parser.add_argument('-u', '--url',
+	                    default='sqlite:////%s/.game.sqlite3'%(os.environ['HOME']),
 	                    help='Port to listen on for http connections')
 	args = parser.parse_args()
 	return args
 
 
 def main(args):
-    logging.basicConfig(filename='/tmp/game_log.txt',level=logging.INFO)
-    server = tornado.web.Application([
-        (r"/", MainHandler),
-        (r"/login", LoginHandler),
-    ])
-    server.listen(args.port)
-    tornado.ioloop.IOLoop.current().start()
+    try:
+        with db.Connect(args.url) as storage:
+            logging.basicConfig(filename='/tmp/game_log.txt',level=logging.INFO)
+            server = tornado.web.Application([
+                (r"/", MainHandler, dict(storage=storage)),
+                (r"/user/(.*)", MainHandler, dict(storage=storage)),
+                (r"/login", LoginHandler, dict(storage=storage)),
+            ])
+            server.listen(args.port)
+            tornado.ioloop.IOLoop.current().start()
+    except:
+        raise
+    finally:
+        pass
 
 
 if __name__ == '__main__':
     main(parse_args())
+
--- ui/index.html.mako
+++ ui/index.html.mako
@@ -7,7 +7,7 @@
 		% if logged_in:
 			Welcome!
 		% else:
-			<form action="/login">
+			<form action="/login" method="POST">
 				<input name="email" placeholder="name@email.com"/>
 				<input name="password" type="password" placeholder="password"/>
 				<input type="submit" value="login">
